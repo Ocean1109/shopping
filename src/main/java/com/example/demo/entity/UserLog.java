5ec8143d64bd65d29e7443d89edd6ee0c105fda0
belancynod <812929007@qq.com>
2021-09-02 15:48:24 +0800

    9.2

--- src/main/java/com/example/demo/entity/UserLog.java
+++ src/main/java/com/example/demo/entity/UserLog.java
@@ -11,20 +11,20 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserLog {
     @TableId(value = "id", type = IdType.AUTO)
-    private int id;
-    private int userId;
-    private int itemId;
-    private int catId;
-    private int merchantId;
-    private int brandId;
-    private int month;
-    private int day;
-    private int action;
-    private int age_range;
-    private int gender;
+    private String id;
+    private String userId;
+    private String itemId;
+    private String catId;
+    private String merchantId;
+    private String brandId;
+    private String month;
+    private String day;
+    private String action;
+    private String ageRange;
+    private String gender;
     private String province;
 
-    public UserLog(int userId, int itemId, int catId, int merchantId, int brandId, int month, int day, int action, int age_range, int gender, String province) {
+    public UserLog(String userId, String itemId, String catId, String merchantId, String brandId, String month, String day, String action, String ageRange, String gender, String province) {
         this.userId = userId;
         this.itemId = itemId;
         this.catId = catId;
@@ -33,7 +33,7 @@ public class UserLog {
         this.month = month;
         this.day = day;
         this.action = action;
-        this.age_range = age_range;
+        this.ageRange = ageRange;
         this.gender = gender;
         this.province = province;
     }
